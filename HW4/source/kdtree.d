33fb6ade89854e1bf9f3f111b6d12bf1ee390ee2
BrianErichsen <142742037+BrianErichsen@users.noreply.github.com>
2024-06-20 15:46:23 -0600

    Complete implementation of kdtree

--- HW4/source/kdtree.d
+++ HW4/source/kdtree.d
@@ -4,10 +4,10 @@
 
 module kdtree;
 
+import std.stdio;
 import common;
 import std.algorithm : partition;
 import std.math;
-//import std.typecons : tuple;
 import std.container : BinaryHeap;
 
 alias P2 = Point!2;
@@ -29,14 +29,31 @@ struct KDTree {
 
         //constructor for Node class
         this(Point!2[] points, AABB!2 aabb) {
+            writeln("Creating node with points: ", points);
             this.aabb = aabb;
+            // Ensures points array is not empty
+            if (points.length == 0) {
+                writeln("Error: Trying to create a node with empty points array!");
+                return;
+            }
+            //base case if list contains only 1 point
+            if (points.length == 1) {
+                point = points[0];
+            }
             //find the median point along the split dimension
-            auto median = points.medianByDimension!splitDimension;
+            //left half points
+            auto leftPoints = points.medianByDimension!splitDimension;
             // '$' means length of array when slicing
-            this.point = median[$ / 2];
+            // uses the median for slicing
+            //this.point = leftPoints[$ / 2];
+            this.point = points[leftPoints.length];
+            writeln("Selected median point: ", this.point);
             //partition points into left and right based on median
-            auto rightPoints = points.partitionByDimension!splitDimension(point[splitDimension]);
-            auto leftPoints = points[0 .. $ - rightPoints.length];
+            auto rightPoints = points[points.length / 2 + 1 .. $];
+            //leftPoints = points[0 .. $ / 2];
+            //auto leftPoints = points[0 .. $ - rightPoints.length];
+            writeln("Left points: ", leftPoints);
+            writeln("Right points: ", rightPoints);
 
             // Creates left child node if there are points on left side
             if (leftPoints.length > 0) {
@@ -56,9 +73,12 @@ struct KDTree {
     private Node!0 root;
 
     this(Point!2[] points) {
+        writeln("Creating KDTree with points: ", points);
         auto aabb = boundingBox(points);
+        writeln("Bounding box: ", aabb);
         root = new Node!0(points, aabb);
     }
+
     //finds all points within certain distance r from point p
     P2[] rangeQuery(P2 p, float r) {
         P2[] result;//empty array to store points
@@ -66,17 +86,18 @@ struct KDTree {
         //template parameter that allows function to be called with nodes
         //of different split dimentions
         void recurse(NodeType)(NodeType node) {
+            //consider point first
             //return early since no points are within desired distance
-            if (distance(node.aabb.closest(p), p) > r) return;
+            if (distance(node.aabb.closest(p), p) >= r) return;
             //if its a leaf node, check the point stored in the node
-            if (node.left is null && node.right is null) {
+            //if (node.left is null && node.right is null) {
                 // stores result if within distance
-                if (distance(p, node.point) <= r) result ~= node.point;
-            } else {
+            if (distance(p, node.point) <= r) result ~= node.point;
+            //} else {
                 //if its not a leaf node, recurse child nodes
-                if (node.left !is null) recurse(node.left);
-                if (node.right !is null) recurse(node.right);
-            }
+            if (node.left !is null) recurse(node.left);
+            if (node.right !is null) recurse(node.right);
+            //}
         }//end of recurse method
         recurse(root);//recurse from root
         return result;//returns array of points within distance
@@ -91,7 +112,7 @@ struct KDTree {
         //template parameter that allows function to be called with nodes
         //of different split dimentions
         void recurse(NodeType)(NodeType node) {
-            //if its leaf insert points into queue
+            //if its leaf insert points into priority queue
             if (node.left is null && node.right is null) {
                 pq.insert(node.point);
             } else {
@@ -108,12 +129,23 @@ struct KDTree {
     }
 }//end of struct KDTree
 
+// combined range query and knn query in 1 unittest for simplification
 unittest {
     Point!2[] points = [Point!2([0.5, 0.5]), Point!2([0.7, 0.7]), Point!2([0.2, 0.3]), Point!2([0.9, 0.9])];
     auto kd = KDTree(points);
     auto rqResults = kd.rangeQuery(Point!2([0.5, 0.5]), 0.3);
+    writeln("KD Range Query Length: " ,rqResults.length);
     assert(rqResults.length == 2);
 
     auto knnResults = kd.knnQuery(Point!2([0.5, 0.5]), 2);
+    writeln("KD KNNQuery Length: ", knnResults.length);
     assert(knnResults.length == 2);
+
+    Point!2[] points2 =  [Point!2([0.1, 0.1]), Point!2([0.9, 0.9]), Point!2([0.3, 0.4]), Point!2([0.8, 0.2])];
+    auto kd2 = KDTree(points2);
+    auto rqResults2 = kd2.rangeQuery(Point!2([0.5, 0.5]), 0.4);
+    assert(rqResults2.length == 1);
+
+    auto knnResults2 = kd2.knnQuery(Point!2([0.5, 0.5]), 1);
+    assert(knnResults2.length == 1);
 }
