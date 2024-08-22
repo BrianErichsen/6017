b05c2df9c5b29045dc7a9392f7e0faf52b773c86
BrianErichsen <142742037+BrianErichsen@users.noreply.github.com>
2024-08-22 16:59:36 -0600

    .

--- Capstone/nn.py
+++ Capstone/nn.py
@@ -5,23 +5,37 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
 
 #data = pd.read_csv('stock_data.csv')
+# fetches data from excel file
 excel_data = pd.read_excel('historical_prices.xlsx', sheet_name=None)
-combined_data = pd.DataFrame()
+combined_data = pd.DataFrame() # initiate empty data that will be combined
 
+# per each sheet in excell file set stock to specific stock and concatenate data
 for sheet_name, df in excel_data.items():
     df['Stock'] = sheet_name
     combined_data = pd.concat([combined_data, df], ignore_index=True)
 
+#print(combined_data.info())
+#print(combined_data.columns)
+#print(combined_data.head())
+#print(combined_data.tail())
+
+# this step is crucial for NN LSTM training
 scaler = MinMaxScaler(feature_range=(0, 1))
 
 scaled_data = scaler.fit_transform(combined_data[['Close', 'Volume']])
 
 sequence_length = 60
 x_train, y_train = [],[]
+# splits x_t and y_t -- 3d Array (len of data, 60, 1)
+# Len of data number of training examples, sequence len is number of steps, 1 feature
+# I think currently only 1D array for y_train
 for i in range(sequence_length, len(scaled_data)):
     x_train.append(scaled_data[i-sequence_length:i, 0])
     y_train.append(scaled_data[i, 0])
+
 x_train, y_train = np.array(x_train), np.array(y_train)
+## cant reshape 154560 into 60, 1
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 model = Sequential([
     LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)),
--- Capstone/stocks.py
+++ Capstone/stocks.py
@@ -1,18 +1,13 @@
 import pandas as pd
-import datetime
-import time
 import yfinance as yf
 
 ## it saves file for now in excel -- convert to csv or download straight csv
 # SMA EMA, RSI, MACD can be calculated from price data - help capture market trends and
 # momentum
 
-## closing prices is what we looking for - stock prices
-
 ## how to tell if stock is overvalued or undervalued P/E ratio, P/B ratio, dividend yield
-
+# states which stocks data to download
 tickers = ['TSLA', 'MSFT', 'GOOG', 'AAPL']
-
 start_date = '2022-01-01'
 end_date = '2024-08-19'
 
